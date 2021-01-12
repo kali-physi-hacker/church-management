8648eb02f77d7f533d366263efdd9e8a89aa4041
Desmond Brown <browndesmond30@yahoo.com>
2021-01-12 15:04:22 +0000

    Kali: Insignificant changes

--- backend/church_management/settings.py
+++ backend/church_management/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "membership"
+    "membership.apps.MembershipConfig"
 ]
 
 MIDDLEWARE = [
