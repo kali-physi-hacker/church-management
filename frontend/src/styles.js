41ea9b8ff563c7306cc771aad98535b0ad177008
Desmond Brown <browndesmond30@yahoo.com>
2021-01-20 22:09:25 +0000

    Kali: Implemented a single source of truth for css files

---
+++ frontend/src/styles.js
@@ -0,0 +1,12 @@
+import "./assets/fonts.googleapis.com/css6079.css?family=Poppins:300,400,500,600,700"
+import "./assets/plugins/custom/fullcalendar/fullcalendar.bundle9c7f.css?v=2.0.5"
+
+import "./assets/css/pages/wizard/wizard-49c7f.css"
+import "./assets/plugins/global/plugins.bundle9c7f.css?v=2.0.5"
+import "./assets/plugins/custom/prismjs/prismjs.bundle9c7f.css?v=2.0.5"
+import "./assets/css/style.bundle9c7f.css?v=2.0.5"
+
+import "./assets/css/themes/layout/header/base/light.css"
+import "./assets/css/themes/layout/header/menu/light.css"
+import "./assets/css/themes/layout/brand/dark.css"
+import "./assets/css/themes/layout/aside/dark.css"
