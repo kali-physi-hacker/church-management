a67a8ba87e747ef90ba581af92a892f3e1ed8478
Desmond Brown <browndesmond30@yahoo.com>
2021-01-20 22:04:15 +0000

    Kali: Added a bunch of Resources Icons

---
+++ frontend/src/Resources/Icons/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Icon = props => {
+    return (
+        <span className={`svg-icon ${props.active ? `svg-icon-${props.color}` : ""} svg-icon-${props.size} svg-icon-${props.color}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px"
+                 height={props.height} viewBox={props.viewBox} version="1.1">
+                <g stroke={"none"} strokeWidth={"1"} fill={"none"}
+                   fillRule={"evenodd"}>
+                    {props.children}
+                </g>
+            </svg>
+        </span>
+    )
+}
+
+
+export default Icon;
