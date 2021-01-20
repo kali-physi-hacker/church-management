90d29dd905e8a921ecafa66092ed1225ea610317
Desmond Brown <browndesmond30@yahoo.com>
2021-01-20 22:12:01 +0000

    Kali: Added boiler code for different screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import ScreenLayout from "./components/Layout";
+import Routes from "./routes";
+
+// Styles, Plugins and Fonts
+import "./styles"
 
 function App() {
   return (
-    <></>
+    <div id="kt_body" className="quick-panel-right demo-panel-right offcanvas-right header-fixed header-mobile-fixed subheader-enabled aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+      <ScreenLayout>
+          <Routes />
+      </ScreenLayout>
+    </div>
   );
 }
 
