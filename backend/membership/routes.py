2d8a76244dd2970d71655d3eb8406acfbe6fb496
Desmond Brown <browndesmond30@yahoo.com>
2021-01-22 17:22:12 +0000

    Kali: Added member endpoint routes

--- backend/membership/routes.py
+++ backend/membership/routes.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from .views.ministry import MinistryListView, MinistryDetailView
+from .views.member import MemberListView, MemberDetailView
 
 
 urlpatterns = [
     path("ministry/", MinistryListView.as_view(), name="ministry_list"),  # List(GET) and Create(POST)
     path("ministry/<int:pk>/", MinistryDetailView.as_view(), name="ministry_detail"),
+
+    path("member/", MemberListView.as_view(), name="member_list"),
+    path("member/<int:pk>/", MemberDetailView.as_view(), name="member_detail"),
 ]
 
 app_name = "membership"
