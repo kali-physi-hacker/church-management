84b8069c2d5ed5f52c7ad6f2a1ebcdda9ad2179c
Desmond Brown <browndesmond30@yahoo.com>
2021-01-24 08:48:21 +0000

    Kali: Added support for redux and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
+import {Provider} from "react-redux";
+
+import store from "./redux/store";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
