0f79230d8e805caf4fd2c6c9aeccfebd80b9ccc4
Desmond Brown <browndesmond30@yahoo.com>
2021-02-06 13:09:45 +0000

    Added cors configurations to solve cors problems during development

--- backend/church_management/settings_dev.py
+++ backend/church_management/settings_dev.py
@@ -1,5 +1,11 @@
 from .settings import *
 
+
+INSTALLED_APPS += ["corsheaders"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CSRF_TRUSTED_ORIGINS = ["localhost:3000", "127.0.0.1:3000"]
+
+
 ALLOWED_HOSTS = ["*"]
 SECRET_KEY = "oxbes^t9@8+t!s8898jh!&b1s04aj=s8^0qeg1js8g4iow0(h+"
 COOKIE_SECURE = False
