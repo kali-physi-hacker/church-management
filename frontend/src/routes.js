32dc23be33f58d568e7509bfa5d9f193c8805295
Desmond Brown <browndesmond30@yahoo.com>
2021-02-19 17:36:23 +0000

    Added routes to new pages added

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,18 @@
 import {Route, Switch} from 'react-router-dom';
 
-import AddPage from "./screens/Members/Add";
+import memberAdd from "./screens/Members/Add";
+import memberList from "./screens/Members/List";
+import ministriesList from "./screens/Ministry/List";
+import ministriesAdd from "./screens/Ministry/Add/index.jsx";
 
 
 const Routes = () => {
     return (
         <Switch>
-            <Route path={"/member/add"} component={AddPage} />
+            <Route path={"/member/add"} component={memberAdd} />
+            <Route path={"/member/list"} component={memberList} />
+            <Route path={"/ministry/list/"} component={ministriesList} />
+            <Route path={"/ministry/add/"} component={ministriesAdd} />
         </Switch>
     )
 }
