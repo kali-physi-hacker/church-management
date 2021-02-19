c199ad754ee452b79dbbffdfcaa6498feacc39bf
Desmond Brown <browndesmond30@yahoo.com>
2021-02-19 17:32:28 +0000

    Added authentication urls and refactored

--- backend/church_management/urls.py
+++ backend/church_management/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("membership.routes"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("membership.routes")),
+    path("api/", include("authentication.routes")),
+]
