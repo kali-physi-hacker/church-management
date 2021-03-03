b8de962a84084f64ff9ff2f582543c14270d39bc
Desmond Brown <browndesmond30@yahoo.com>
2021-03-03 22:21:12 +0000

    Created a symbolic link to the index.html file rather than copy it

--- backend/church_management/settings.py
+++ backend/church_management/settings.py
@@ -83,7 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
